23cbca128b29c777928ffd26ce8bf81dec19cc06
Thomas Hallock <hallock@gmail.com>
2016-08-20 04:21:38 -0700

    moved context-based function to 'fromContext' static and working with passed kit parts instead. exporting complete component as default.

--- index.js
+++ index.js
@@ -3,39 +3,45 @@ const flow = require('lodash.flow');
 const path = require('path');
 const values = require('object.values');
 const hoist = require('hoist-non-react-statics');
-module.exports = function (context) {
-    const uniquePaths = context.keys().reduce((obj, key) => {
-        const existingKey = obj[context.resolve(key)];
-        if (existingKey && existingKey.length <= key.length) {
-            return obj;
-        }
-        return Object.assign({}, obj, {
-            [context.resolve(key)]: key
-        })
-    }, {});
-    const moduleMap = values(uniquePaths).reduce((obj, key) => {
-        let contextForKey = context(key);
-        if (contextForKey.default) {
-            contextForKey = contextForKey.default;
-        }
-        return Object.assign({}, obj, {
-            [path.relative('.', key)]: contextForKey
-        })
-    }, {});
+const buildKit = module.exports = function (parts) {
     let wrappersList = ['redux', 'relay'];
-    if (moduleMap.index && moduleMap.index.wrappers) {
-        wrappersList = moduleMap.index.wrappers
+    if (parts.index && parts.index.wrappers) {
+        wrappersList = parts.index.wrappers
     }
-    wrappersList = wrappersList.filter(moduleName => moduleMap[moduleName]);
-    return Object.assign({}, moduleMap,
+    wrappersList = wrappersList.filter(moduleName => parts[moduleName]);
+    const complete = wrappersList.reduce(
+        (complete, moduleName) => hoist(parts[moduleName](complete), complete),
+        parts.component
+    );
+    return Object.assign(complete, parts,
         {
-            default: wrappersList.reduce(
-                (complete, moduleName) => hoist(moduleMap[moduleName](complete), complete),
-                moduleMap.component
-            )
+            default: complete
         },
         wrappersList.reduce((obj, moduleName) => Object.assign({}, obj, {
-            [`${moduleName}_wrapped`]: hoist(moduleMap[moduleName](moduleMap.component), moduleMap.component)
+            [`${moduleName}_wrapped`]: hoist(parts[moduleName](parts.component), parts.component)
         }), {})
     );
 };
+Object.assign(module.exports, {
+    fromContext: function (context) {
+        const uniquePaths = context.keys().reduce((obj, key) => {
+            const existingKey = obj[context.resolve(key)];
+            if (existingKey && existingKey.length <= key.length) {
+                return obj;
+            }
+            return Object.assign({}, obj, {
+                [context.resolve(key)]: key
+            })
+        }, {});
+        const parts = values(uniquePaths).reduce((obj, key) => {
+            let contextForKey = context(key);
+            if (contextForKey.default) {
+                contextForKey = contextForKey.default;
+            }
+            return Object.assign({}, obj, {
+                [path.relative('.', key)]: contextForKey
+            })
+        }, {});
+        return buildKit(parts);
+    }
+});
--- test/spec.js
+++ test/spec.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const fs = require('fs');
 const execSync = require('child_process').execSync;
 const dedent = require('dedent');
-const kitBuilder = require('..');
+const buildKit = require('..');
 const path = require('path');
 const rimraf = require('rimraf').sync;
 const mkdirp = require('mkdirp').sync;
@@ -15,85 +15,98 @@ const buildBundle = () => {
     delete require.cache[require.resolve(bundlePath)];
 };
 describe('component kit builder', function () {
-    beforeEach(function () {
-        rimraf(testTmp);
-        mkdirp(testTmp);
-        mkdirp(myComponentDir);
-        fs.writeFileSync(path.join(myComponentDir, 'component.js'), `module.exports = x => \`component \${x}\`;`);
-        fs.writeFileSync(path.join(myComponentDir, 'redux.js'), `module.exports = x => y => x(\`redux \${y}\`);`);
-        fs.writeFileSync(path.join(myComponentDir, 'relay.js'), `module.exports = x => y => x(\`relay \${y}\`);`);
-        fs.writeFileSync(path.join(testTmp, 'build-manager.js'), dedent`
-            const kitBuilder = require('${path.relative(testTmp, require.resolve('..'))}')
-            module.exports = kitBuilder(require.context('./${path.relative(testTmp, myComponentDir)}/'));
-        `);
+    it('should compose the components', function () {
+        const kit = buildKit({
+            component: x => `component ${x}`,
+            redux: x => y => x(`redux ${y}`),
+            relay: x => y => x(`relay ${y}`),
+        });
+        assert.strictEqual(kit.default, kit);
+        assert.equal(kit.redux_wrapped('bar'), 'component redux bar');
+        assert.equal(kit.relay_wrapped('bar'), 'component relay bar');
+        assert.equal(kit('bar'), 'component redux relay bar');
     });
-    afterEach(function () {
-        rimraf(testTmp);
-        rimraf(myComponentDir);
+    describe('fromContext', function () {
+        beforeEach(function () {
+            rimraf(testTmp);
+            mkdirp(testTmp);
+            mkdirp(myComponentDir);
+            fs.writeFileSync(path.join(myComponentDir, 'component.js'), `module.exports = x => \`component \${x}\`;`);
+            fs.writeFileSync(path.join(myComponentDir, 'redux.js'), `module.exports = x => y => x(\`redux \${y}\`);`);
+            fs.writeFileSync(path.join(myComponentDir, 'relay.js'), `module.exports = x => y => x(\`relay \${y}\`);`);
+            fs.writeFileSync(path.join(testTmp, 'build-manager.js'), dedent`
+                const buildKit = require('${path.relative(testTmp, require.resolve('..'))}');
+                module.exports = buildKit.fromContext(require.context('./${path.relative(testTmp, myComponentDir)}/'));
+            `);
+        });
+        afterEach(function () {
+            rimraf(testTmp);
+            rimraf(myComponentDir);
+        });
+        it('should cope with missing redux', function () {
+            rimraf(path.join(myComponentDir, 'redux.js'));
+            buildBundle();
+            assert.deepEqual(
+                [
+                    'relay',
+                    'relay_wrapped',
+                    'component',
+                    'default'
+                ].sort(),
+                Object.keys(require(bundlePath)).sort()
+            );
+        });
+        it('should cope with missing relay', function () {
+            rimraf(path.join(myComponentDir, 'relay.js'));
+            buildBundle();
+            assert.deepEqual(
+                [
+                    'redux',
+                    'redux_wrapped',
+                    'component',
+                    'default'
+                ].sort(),
+                Object.keys(require(bundlePath)).sort()
+            );
+        });
+        it('should export properties based on the files present in the component directory', function () {
+            buildBundle();
+            assert.deepEqual(
+                [
+                    'relay',
+                    'redux',
+                    'relay_wrapped',
+                    'redux_wrapped',
+                    'component',
+                    'default'
+                ].sort(),
+                Object.keys(require(bundlePath)).sort()
+            );
+        });
+        it('should compose all the composable wrappers together', function () {
+            buildBundle();
+            assert(require(bundlePath).default(), 'redux relay component');
+        });
+        it('should work with es6-style default exports', function () {
+            fs.writeFileSync(path.join(myComponentDir, 'redux.js'), `module.exports = {default: x => y => y(\`redux \${x}\`)};`);
+            buildBundle();
+            require(bundlePath);
+        });
+        it('should hoist static properties through composition', function () {
+            fs.writeFileSync(path.join(myComponentDir, 'component.js'), `
+            module.exports = Object.assign(x => \`component \${x}\`, {componentStaticProp: true});
+            `);
+            fs.writeFileSync(path.join(myComponentDir, 'redux.js'), `
+            module.exports = x => Object.assign(y => y(\`redux \${x}\`), {reduxStaticProp: true});
+            `);
+            fs.writeFileSync(path.join(myComponentDir, 'relay.js'), `
+            module.exports = x => Object.assign(y => y(\`relay \${x}\`), {relayStaticProp: true});
+            `);
+            buildBundle();
+            const bundle = require(bundlePath);
+            assert(bundle.default.reduxStaticProp);
+            assert(bundle.default.relayStaticProp);
+            assert(bundle.default.componentStaticProp);
+        })
     });
-    it('should cope with missing redux', function () {
-        rimraf(path.join(myComponentDir, 'redux.js'));
-        buildBundle();
-        assert.deepEqual(
-            [
-                'relay',
-                'relay_wrapped',
-                'component',
-                'default'
-            ].sort(),
-            Object.keys(require(bundlePath)).sort()
-        );
-    });
-    it('should cope with missing relay', function () {
-        rimraf(path.join(myComponentDir, 'relay.js'));
-        buildBundle();
-        assert.deepEqual(
-            [
-                'redux',
-                'redux_wrapped',
-                'component',
-                'default'
-            ].sort(),
-            Object.keys(require(bundlePath)).sort()
-        );
-    });
-    it('should export properties based on the files present in the component directory', function () {
-        buildBundle();
-        assert.deepEqual(
-            [
-                'relay',
-                'redux',
-                'relay_wrapped',
-                'redux_wrapped',
-                'component',
-                'default'
-            ].sort(),
-            Object.keys(require(bundlePath)).sort()
-        );
-    });
-    it('should compose all the composable wrappers together', function () {
-        buildBundle();
-        assert(require(bundlePath).default(), 'redux relay component');
-    });
-    it('should work with es6-style default exports', function () {
-        fs.writeFileSync(path.join(myComponentDir, 'redux.js'), `module.exports = {default: x => y => y(\`redux \${x}\`)};`);
-        buildBundle();
-        require(bundlePath);
-    });
-    it('should hoist static properties through composition', function () {
-        fs.writeFileSync(path.join(myComponentDir, 'component.js'), `
-        module.exports = Object.assign(x => \`component \${x}\`, {componentStaticProp: true});
-        `);
-        fs.writeFileSync(path.join(myComponentDir, 'redux.js'), `
-        module.exports = x => Object.assign(y => y(\`redux \${x}\`), {reduxStaticProp: true});
-        `);
-        fs.writeFileSync(path.join(myComponentDir, 'relay.js'), `
-        module.exports = x => Object.assign(y => y(\`relay \${x}\`), {relayStaticProp: true});
-        `);
-        buildBundle();
-        const bundle = require(bundlePath);
-        assert(bundle.default.reduxStaticProp);
-        assert(bundle.default.relayStaticProp);
-        assert(bundle.default.componentStaticProp);
-    })
 });
